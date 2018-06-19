06855f59cf0efd894f90112c151bf23315df2c3b
wtseng678 <wtseng678>
2018-06-18 20:32:50 -0700

    First class created

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,22 +1,4 @@
-/*var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
-};*/
 
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
@@ -32,7 +14,21 @@ makeBlinkyDancer.prototype.step = function() {
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  $(this.$node).css("color", "blue");
+  //this.$node.css("color", "green");
   //this.$node.toggleClass("blue");
+  
+  //$('.dancer').css('border', '10px solid red');
   this.$node.toggle();
+};
+
+// For lining up dancers on the sides
+makeBlinkyDancer.prototype.lineUp = function() {
+  var styleSettings = {
+    left: 0,
+  };
+  this.$node.css(styleSettings);
+};
+
+makeBlinkyDancer.prototype.jump = function() {
+  this.setPosition($('body').height() * Math.random(), $('body').width() * Math.random());
 };
--- src/legionDancer.js
+++ src/legionDancer.js
@@ -7,9 +7,39 @@ var legionDancer = function(top, left, timeBetweenSteps) {
 legionDancer.prototype = Object.create(makeDancer.prototype);
 legionDancer.prototype.constructor = legionDancer;
 
+legionDancer.prototype.step = function() {
+  this.oldStep = makeDancer.prototype.step;
+  this.oldStep();
+  var randColor = function() {
+    return Math.floor(Math.random() * 256);
+  };
+  
+  var styleSettings = {
+    border: '10px solid rgb(' + randColor() + ',' + randColor() + ',' + randColor() + ')' 
+  };
+  //this.lineUp(); 
+  this.$node.css(styleSettings);
+  this.$node.toggle(); 
+  this.jump();
+};
+
 legionDancer.prototype.changeColors = function() {
   
   this.changeC = makeDancer.prototype.changeColors;
   this.changeC(); 
   
+  
+  $('.dancer').css('border', '10px solid blue');
+  this.$node.toggle();
+
+};
+
+legionDancer.prototype.lineUp = function() {
+  var styleSettings = {
+    left: 0,
+  };
+  this.$node.css(styleSettings);
+};
+legionDancer.prototype.jump = function() {
+  this.setPosition($('body').height() * Math.random(), $('body').width() * Math.random());
 };
